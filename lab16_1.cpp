136b1ed28662499277d616497c7a7aaecea8c050
PraeThipwarin <prae.tippy@gmail.com>
2024-02-06 13:06:00 +0700

    lab 16_1 complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,44 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i);
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        dPtr[i]  = rand()%101/100.0;
+    }
+}
+
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+	
+	for(int i = 0 ;i < N ; i++){
+		sum1[i] = 0;
+	}
+	
+	for(int i = 0 ;i < N ; i++){
+		for(int j = i*M ; j < M+(i*M) ; j++ ){
+			sum1[i] += dPtr[j];
+		}
+	}
+}
+
+void findColSum(const double *dPtr,double *sum2,int N,int M){
+    for(int i = 0 ;i < M ; i++){
+		sum2[i] = 0;
+	}
+	
+	 for(int i = 0 ;i < M ; i++){
+		for(int j = i ; j < (N*M) ; j += M ){
+			sum2[i] += dPtr[j];
+		}
+	}
+}
